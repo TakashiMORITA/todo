42a489cd82437a218bae44efb13c2c03426c309a
MORITA TAKASHI <tmorita@MORITAnoMacBook-puro.local>
2018-05-26 08:32:41 +0900

    date,tag

--- todo/AddToDo.swift
+++ todo/AddToDo.swift
@@ -11,18 +11,39 @@ var todoItem = [Any]()
 var todoTime = [Any]()
 
 class AddToDo: UIViewController {
-
+    
+    //締め切り用テキストフィールド
+    @IBOutlet weak var timeText: UITextField!
+    var toolBar:UIToolbar!
+    
+    //タスク用テキストフィールド
     @IBOutlet weak var itemText: UITextField!
     
-    @IBOutlet weak var datePicker: UIDatePicker!
+    //datePickerで締め切りが選択できるようにする
+    @IBAction func timeTextField(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(datePickerValueChanged), for: UIControlEvents.valueChanged)
+    }
+    
+    //datepickerが選択されたらtextfieldにe表示
+    @objc func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat  = "MM/dd";
+        timeText.text = dateFormatter.string(from: sender.date)
+    }
+    
+    //toolbarのdoneボタン
+    @objc func doneBtn(){
+        timeText.resignFirstResponder()
+    }
     
     @IBAction func addItem(_ sender: UIBarButtonItem) {
         if(itemText.text != ""){
         todoItem.append(itemText.text!)
-        todoTime.append("\(datePicker.date)")
         itemText.text = ""
         UserDefaults.standard.set(todoItem, forKey: "todoList")
-        
         }
     }
     
@@ -34,6 +55,14 @@ class AddToDo: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //datepicker上のtoolbarのdoneボタン
+        toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let toolBarBtn = UIBarButtonItem(title: "完了", style: .plain, target: self, action: #selector(doneBtn))
+        toolBar.items = [toolBarBtn]
+        timeText.inputAccessoryView = toolBar
+
     }
 
     override func didReceiveMemoryWarning() {
